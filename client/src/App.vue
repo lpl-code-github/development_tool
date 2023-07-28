dd49e4d5c805856fafbdeac86a326fca50d2e450
李鹏龙 <819508292@qq.com>
2023-07-29 04:21:06 +0800

    App.vue设置高度为可视高度，增加新的菜单（日志），增加页面禁止滚动，仅router-view显示区域可以滚动

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,92 +1,107 @@
 <template>
-  <a-layout id="components-layout-demo-side" style="min-height: 100vh">
-    <a-layout-sider v-model="collapsed" collapsible>
-      <div class="logo_img">
-        <img class="logo" src="./assets/r1_logo.png" style="background: none"/>
-      </div>
+  <div class="app">
+    <a-layout id="components-layout-demo-side" style="min-height: 100vh!important;">
+      <a-layout-sider v-model="collapsed" collapsible>
+        <div class="logo_img">
+          <img class="logo" src="./assets/r1_logo.png" style="background: none"/>
+        </div>
 
-      <a-menu theme="dark" v-model="selectedKeys" :default-selected-keys="defaultSelectedKeys" mode="inline"
-              :forceSubMenuRender="true"
-              >
-          <a-menu-item v-for="item in simpleSidebarData" :key="item.key" >
+        <!--  导航栏 这里应该拆成组件 -->
+        <a-menu theme="dark" v-model="selectedKeys" :default-selected-keys="defaultSelectedKeys" mode="inline"
+                :forceSubMenuRender="true"
+        >
+          <!-- 一级菜单 -->
+          <a-menu-item v-for="item in simpleSidebarData" :key="item.key">
             <router-link :to="item.link"></router-link>
-            <a-icon :type="item.icon"  />
+            <a-icon :type="item.icon"/>
             <span>{{ item.text }}</span>
           </a-menu-item>
+          <!-- 多级菜单 -->
           <a-sub-menu v-for="item in subSidebarData" :key="item.key">
-          <span slot="title">
-            <a-icon :type="item.icon" />
-            <span>{{ item.text }}</span>
-          </span>
+            <span slot="title">
+              <a-icon :type="item.icon"/>
+              <span>{{ item.text }}</span>
+            </span>
             <a-menu-item v-for="child in item.children" :key="child.key">
               <router-link :to="child.link"></router-link>
               {{ child.text }}
             </a-menu-item>
           </a-sub-menu>
-      </a-menu>
+        </a-menu>
 
-    </a-layout-sider>
-    <a-layout>
-      <MyHeader></MyHeader>
+      </a-layout-sider>
+      <a-layout style="height: 100vh">
+        <!--  头部组件  -->
+        <MyHeader></MyHeader>
 
-      <a-layout-content style="margin: 10px 16px">
-        <a-breadcrumb>
-          <span>/&nbsp;&nbsp;</span>
-          <a-breadcrumb-item v-for="item in breadcrumb" :key="item">
-            {{ item }}
-          </a-breadcrumb-item>
-        </a-breadcrumb>
+        <!-- 显示内容  -->
+        <a-layout-content style="margin: 10px 16px;">
+          <a-breadcrumb>
+            <span>/&nbsp;&nbsp;</span>
+            <a-breadcrumb-item v-for="item in breadcrumb" :key="item">
+              {{ item }}
+            </a-breadcrumb-item>
+          </a-breadcrumb>
 
-        <router-view></router-view>
-      </a-layout-content>
-      <a-layout-footer style="text-align: center">
-        Ant Design ©2018 Created by Ant UED
-      </a-layout-footer>
+          <router-view style="height:100%;overflow-y: auto"></router-view>
+        </a-layout-content>
+        <a-layout-footer style="text-align: center">
+          Ant Design ©2018 Created by Ant UED
+        </a-layout-footer>
+      </a-layout>
     </a-layout>
-  </a-layout>
+  </div>
 </template>
+
 <script>
 import MyHeader from "@/components/common/MyHeader";
+
 export default {
   name: "App",
   components: {MyHeader},
   data() {
     return {
       collapsed: false,
-      defaultSelectedKeys:['1'],
-      breadcrumb:[],
-      selectedKeys:[],
+      defaultSelectedKeys: ['1'],
+      breadcrumb: [],
+      selectedKeys: [],
       simpleSidebarData: [
         {
           key: "1",
           icon: "code",
-          text: "脚本",
-          link:'/script'
+          text: "日志",
+          link: '/index'
         },
         {
           key: "2",
+          icon: "code",
+          text: "脚本",
+          link: '/script'
+        },
+        {
+          key: "3",
           icon: "bug",
           text: "调试",
-          link:'/debug'
+          link: '/debug'
         },
       ],
       subSidebarData: [
         {
-          key: "3",
+          key: "4",
           icon: "branches",
           text: "测试",
           children: [
-            { key: "3-1", text: "数据库备份", link: "/backup"},
-            { key: "3-2", text: "Newman", link: "/newman"}
+            {key: "4-1", text: "数据库备份", link: "/backup"},
+            {key: "4-2", text: "Newman", link: "/newman"}
           ]
         },
         {
-          key: "4",
+          key: "5",
           icon: "codepen",
           text: "生成器",
           children: [
-            { key: "4-1", text: "代码生成器", link: "/code-generator"},
-            { key: "4-2", text: "Slate生成器", link: "/slate-generator" }
+            {key: "5-1", text: "代码生成器", link: "/code-generator"},
+            {key: "5-2", text: "Slate生成器", link: "/slate-generator"}
           ]
         }
       ]
@@ -144,9 +159,14 @@ export default {
   background: rgba(255, 255, 255, 0.2);
   margin: 16px;
 }
-.logo_img{
-  display: flex !important;
-  justify-content: center!important;
 
+.logo_img {
+  display: flex !important;
+  justify-content: center !important;
+
+}
+.app{
+  max-height: 100vh;
+  overflow-y: hidden;
 }
 </style>
