d3c021e7ff869f379539a20b78ed15d68474bfe7
李鹏龙 <819508292@qq.com>
2023-07-29 04:18:04 +0800

    增加首页日历日志功能，并设置了中文语言，取消了组件自带的月/年选择器

---
+++ client/src/Index.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="index">
+    <a-calendar :locale="locale" :value="selectValue" @select="onSelect">
+      <ul slot="dateCellRender" slot-scope="value" class="events" >
+        <li v-for="item in getListData(value)" :key="item.content">
+          <a-badge :status="item.type" :text="item.content" />
+        </li>
+      </ul>
+      <template slot="monthCellRender" slot-scope="value">
+        <div v-if="getMonthData(value)" class="notes-month">
+          <section>{{ getMonthData(value) }}</section>
+          <span>Backlog number</span>
+        </div>
+      </template>
+    </a-calendar>
+  </div>
+</template>
+
+<script>
+import zhCN from 'ant-design-vue/es/locale/zh_CN';//引入antd中文包
+import moment from 'moment';//引入moment
+moment.locale('zh-cn');//配置moment中文环境
+export default {
+  name: "Index",
+  data(){
+    return{
+      locale:zhCN,//传值给a-config-provider组件
+      selectValue: null,
+    }
+  },
+  methods: {
+    getListData(value) {
+      let listData;
+      switch (value.date()) {
+        case 8:
+          listData = [
+            { type: 'warning', content: 'This is warning event.' },
+            { type: 'success', content: 'This is usual event.' },
+          ];
+          break;
+        case 10:
+          listData = [
+            { type: 'warning', content: 'This is warning event.' },
+            { type: 'success', content: 'This is usual event.' },
+            { type: 'error', content: 'This is error event.' },
+          ];
+          break;
+        case 15:
+          listData = [
+            { type: 'warning', content: 'This is warning event' },
+            { type: 'success', content: 'This is very long usual event。。....' },
+            { type: 'error', content: 'This is error event 1.' },
+            { type: 'error', content: 'This is error event 2.' },
+            { type: 'error', content: 'This is error event 3.' },
+            { type: 'error', content: 'This is error event 4.' },
+          ];
+          break;
+        default:
+      }
+      return listData || [];
+    },
+
+    getMonthData(value) {
+      if (value.month() === 8) {
+        return 1394;
+      }
+    },
+
+    onSelect(value) {
+      const date = moment(value);
+      const formattedDate = date.format('YYYY-MM-DD');
+
+      console.log(formattedDate);
+    },
+  },
+}
+</script>
+
+<style scoped>
+.events {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+.events .ant-badge-status {
+  overflow: hidden;
+  white-space: nowrap;
+  width: 100%;
+  text-overflow: ellipsis;
+  font-size: 12px;
+}
+.notes-month {
+  text-align: center;
+  font-size: 28px;
+}
+.notes-month section {
+  font-size: 28px;
+}
+.index{
+  height: 100%;
+}
+/deep/ .ant-radio-group {
+  display: none!important;
+}
+
+</style>
