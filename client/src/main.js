64d68c61ba1dc116981ba40f1fcbf07a92f8d2d3
李鹏龙 <819508292@qq.com>
2023-07-29 04:21:50 +0800

    引入封装好的axios工具

--- client/src/main.js
+++ client/src/main.js
@@ -5,9 +5,14 @@ import 'ant-design-vue/dist/antd.css';
 import router from './router/index'
 // 引入公共css
 import './assets/css/common.css'
+// 引入封装好的axios请求
+import {request} from '@/api/index'
+
+Vue.prototype.$request = request
+
+Vue.config.productionTip = false
 
 Vue.use(Antd);
-Vue.config.productionTip = false
 
 new Vue({
     router,
