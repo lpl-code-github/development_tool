6d3b0ce425cb7b158e868cdd109c48adef8b7415
李鹏龙 <819508292@qq.com>
2023-07-30 22:40:06 +0800

    增加echart依赖

--- client/src/main.js
+++ client/src/main.js
@@ -7,8 +7,22 @@ import router from './router/index'
 import './assets/css/common.css'
 // 引入封装好的axios请求
 import {request} from '@/api/index'
+// 按需引入echarts
+import * as echarts from 'echarts/core';
+import { TooltipComponent, LegendComponent } from 'echarts/components';
+import { PieChart } from 'echarts/charts';
+import { LabelLayout } from 'echarts/features';
+import { CanvasRenderer } from 'echarts/renderers';
+echarts.use([
+    TooltipComponent,
+    LegendComponent,
+    PieChart,
+    CanvasRenderer,
+    LabelLayout
+]);
 
 Vue.prototype.$request = request
+Vue.prototype.$echarts = echarts
 
 Vue.config.productionTip = false
 
