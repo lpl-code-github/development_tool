db59c37e45acf6131bdcb9f0363927d102b6b672
李鹏龙 <819508292@qq.com>
2023-08-06 05:01:33 +0800

    增加新的备份请求的modal框

---
+++ client/src/components/backup/AddBackUp.vue
@@ -0,0 +1,133 @@
+<template>
+  <div>
+    <a-modal v-model="visible" title="Title" on-ok="handleOk" :afterClose="afterClose" :maskClosable="false">
+      <template slot="footer">
+        <a-button key="back" @click="handleCancel">
+          取消
+        </a-button>
+        <a-button key="submit" type="primary" :loading="loading" @click="handleOk">
+          提交
+        </a-button>
+      </template>
+      <a-form-model
+          ref="ruleForm"
+          :model="form"
+          :rules="rules"
+          :label-col="{ span: 3 }"
+          :wrapper-col="{ span: 20}"
+          style="width: 100%">
+        <a-form-model-item ref="name" label="名称" prop="name">
+          <a-input v-model="form.name"/>
+        </a-form-model-item>
+        <a-form-model-item label="描述" prop="desc">
+          <a-input v-model="form.desc" type="textarea"/>
+        </a-form-model-item>
+        <a-form-model-item label="DB" prop="db">
+          <a-select
+              show-search
+              placeholder="选择数据库"
+              option-filter-prop="children"
+              :filter-option="filterOption"
+              v-model="form.db"
+          >
+            <a-select-option v-for="(item,index) in dbList" :key="index" :value="item">
+              {{ item }}
+            </a-select-option>
+          </a-select>
+        </a-form-model-item>
+      </a-form-model>
+    </a-modal>
+  </div>
+</template>
+<script>
+import {postDatabaseBackup} from "@/api/request";
+
+export default {
+  name: "AddBackUp",
+  props: {
+    openFlag: {
+      type: Boolean,
+      required: true
+    },
+    dbList: {
+      type: Array,
+      required: true
+    },
+  },
+  data() {
+    return {
+      loading: false,
+      visible: false,
+      formLayout: 'horizontal',
+      form: {
+        name: '',
+        desc: '',
+        db: ''
+      },
+      rules: {
+        name: [
+          {required: true, message: '请输入操作名称', trigger: 'blur'},
+          {min: 5, max: 50, message: '长度必须在 5 ～ 50', trigger: 'blur'},
+        ],
+        db: [
+          {required: true, message: '请选择数据库', trigger: 'blur'},
+        ],
+      },
+    };
+  },
+  watch: {
+    openFlag: {
+      handler: function (newVal, oldVal) {
+        console.log(newVal)
+        this.visible = newVal
+      },
+      // 深度观察监听
+      deep: true
+    }
+  },
+  mounted() {
+    this.visible = this.openFlag
+  },
+  methods: {
+    handleOk(e) {
+      this.loading = true;
+      this.$refs.ruleForm.validate(valid => {
+        if (valid) {
+          var data = {
+            data:this.form
+          }
+          this.$request.postDatabaseBackup(data).then(res=>{
+            console.log(res)
+          })
+
+
+          this.initFormData()
+          setTimeout(() => {
+            this.$emit('updateModelStatus', false)
+            this.loading = false;
+          }, 1);
+        } else {
+          this.loading = false
+          return false
+        }
+      });
+    },
+    handleCancel(e) {
+      this.initFormData()
+      this.$emit('updateModelStatus', false)
+    },
+    afterClose(e) {
+      this.initFormData()
+      this.$emit('updateModelStatus', false)
+    },
+    filterOption(input, option) {
+      return (
+          option.componentOptions.children[0].text.toLowerCase().indexOf(input.toLowerCase()) >= 0
+      );
+    },
+    initFormData() {
+      this.$refs.ruleForm.resetFields();
+    }
+  },
+};
+</script>
