90d4d36efbe25c60dfe85575a7cb8a3088b88d58
李鹏龙 <819508292@qq.com>
2023-08-13 22:43:35 +0800

    文件上传下载的功能，上传时当出现重名时，按浏览器的方式处理+1

--- server/src/Controller/Functional/FileController.php
+++ server/src/Controller/Functional/FileController.php
@@ -6,6 +6,7 @@ use App\Controller\BaseController;
 use App\Factory\ExceptionFactory;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -27,6 +28,7 @@ class FileController extends BaseController
     /**
      * @Route("/uploadFile", name="上传一个文件", methods={"POST"})
      * // 增加一种type类型 需要在OperationLogFactory的handleUploadFileRewriteActionName中增加一种类型处理
+     * @throws \Exception
      */
     public function executeUpload(Request $request): Response
     {
@@ -35,13 +37,19 @@ class FileController extends BaseController
 
         $file = $request->files->get('file');
         $type = $request->query->get('type') ?? null;
+
         if (!$type) {
             throw ExceptionFactory::WrongFormatException("缺少文件类型type参数");
         }
         if (!$file) {
             throw ExceptionFactory::WrongFormatException("文件上传失败");
         }
-
+        // 检查文件后缀是否合法
+        $extension = $file->getClientOriginalExtension();
+        if (!in_array($extension, $allowedExtensions)) {
+            throw ExceptionFactory::WrongFormatException("不允许上传该类型的文件");
+        }
+        // 检查type是否合法，并且确定保存到哪个目录下
         switch ($type) {
             case "script":
                 $targetDirectory = BASE_PATH . $this->parameterBag->get('script_file_path');
@@ -49,17 +57,13 @@ class FileController extends BaseController
             default:
                 throw ExceptionFactory::WrongFormatException("文件类型type参数不合法");
         }
-        if (file_exists($targetDirectory.$file->getClientOriginalName())){
-            throw ExceptionFactory::WrongFormatException("文件已存在");
-        }
-        // 检查文件后缀是否合法
-        $extension = $file->getClientOriginalExtension();
-        if (!in_array($extension, $allowedExtensions)) {
-            throw ExceptionFactory::WrongFormatException("不允许上传该类型的文件");
-        }
 
         // 获取文件名并确保长度不超过操作系统文件名允许的最大长度
-        $filename = substr($file->getClientOriginalName(), 0, 255 - strlen($extension) - 1);
+        $originalFilename = $file->getClientOriginalName();
+        $filename = substr($originalFilename, 0, 255 - strlen($extension) - 1);
+
+        // 防止文件重名
+        $filename = $this->getAvailableFilename($filename, $targetDirectory);
 
         // 移动上传的文件到目标目录
         $file->move($targetDirectory, $filename);
@@ -102,5 +106,30 @@ class FileController extends BaseController
         $response->headers->set('Content-Type', 'application/octet-stream');
 
         return $response;
+    }
+
+    /**
+     * 查找某目录下所有文件，如果有重名的就在文件名后加数字, 返回一个新的filename
+     * @throws \Exception
+     */
+    private function getAvailableFilename($filename, $targetDirectory): string
+    {
+        $finder = new Finder();
+        $finder->files()->in($targetDirectory);
+        $existingFiles = [];
+        foreach ($finder as $f) {
+            $existingFiles[] = $f->getFilename();
+        }
+
+        $index = 1;
+        $extension = pathinfo($filename, PATHINFO_EXTENSION); // 获取后缀
+        $uniqueFilename = pathinfo($filename, PATHINFO_FILENAME);// 获取文件名
+        $temp = $uniqueFilename;
+        while (in_array($uniqueFilename . '.' . $extension, $existingFiles)) {
+            $uniqueFilename = $temp . ' (' . $index . ')';
+            $index++;
+        }
+
+        return $uniqueFilename.'.'.$extension;
     }
 }
