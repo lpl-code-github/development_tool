d0b3c3089ebe52b9b6ffe92a2b9bec947445b1e1
李鹏龙 <819508292@qq.com>
2023-08-20 00:27:00 +0800

    增加示例json

--- client/src/components/code/Json.vue
+++ client/src/components/code/Json.vue
@@ -19,7 +19,7 @@ export default {
   data() {
     return {
       hasJsonFlag: true, // json是否验证通过
-      jsonText: {}
+      jsonText: ""
     }
   },
 
@@ -27,6 +27,64 @@ export default {
     vueJsonEditor
   },
   created() {
+    if(this.jsonText == ''){
+      this.jsonText = {
+        "example_data": [
+          {
+            "id": "2",
+            "route": {
+              "properties":{
+                "id":1,
+                "path":[
+                  {
+                    "id":1,
+                    "name":"path"
+                  }
+                ]
+              },
+              "info":{
+                "name":{
+                  "full_name":"test",
+                  "last_name":"te"
+                }
+              }
+            },
+            "created_at": "2023-07-30 19:43:04",
+            "updated_at": "2023-07-30 19:43:04"
+          }
+        ],
+        "token_user": {
+          "id": "1",
+          "surname": "Administrator",
+          "forename": "RISKID",
+          "first_name": "RISKID",
+          "full_name": "RISKID Administrator",
+          "name": "Administrator",
+          "token": "a8680ad1e02877f8962171e85a095b6b2c600855",
+          "email": "administrator@riskid.nl",
+          "type": "admin",
+          "login_state": "1",
+          "properties": {
+            "lang": "en"
+          },
+          "hidden_folders": null,
+          "active": "1",
+          "created_at": "2023-07-19 14:27:22",
+          "updated_at": "2023-07-19 14:34:03",
+          "owner_type": "u",
+          "project_order": [
+            [
+              "432"
+            ]
+          ],
+          "is_observing": false
+        }
+      }
+      this.$emit('updateJsonData', {
+        value:this.jsonText,
+        hasJsonFlag:true
+      })
+    }
   },
   methods: {
     onJsonChange(value) {
